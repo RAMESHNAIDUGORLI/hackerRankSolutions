c81fbc7066d1d98105f06bc4f1709021fab1fb9c
RAMESHNAIDUGORLI <rameshnaidu.gorli@gmail.com>
2017-06-20 19:57:10 +0530

    Update ANDxorOR.java

--- Stacks/ANDxorOR.java
+++ Stacks/ANDxorOR.java
@@ -1,4 +1,5 @@
-import java.io.*;
+//In comments code is witout using stack
+/*import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
@@ -32,6 +33,75 @@ public class Solution {
         System.out.println(max);
     }
     public static int returnOperations(int M1 , int M2){
+        int mAnd = M1&M2;
+        //System.out.println(mAnd);
+        int mOr = M1|M2;
+        //System.out.println(mOr);
+        int mXor = mAnd^mOr;
+        int m1Xor = M1^M2;
+        int result = mXor&m1Xor;
+        //System.out.println(result);
+        return result;
+    }
+}*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int [] a = new int[N+1];
+        for(int j = 0; j < N ; j++){
+            a[j] = sc.nextInt();
+        }
+       Stack<Integer> stack = new Stack<Integer>();
+        //stack.push(0);
+        int max = 0;
+        for (int i = 0 ; i < N ; i++){
+            while(true){
+                if(stack.isEmpty()){
+                    stack.push(a[i]);
+                    break;
+                }else if(stack.peek() < a[i]){
+                    int si = returnOperations(stack.peek(), a[i]);
+                    if(max < si){
+                        max = si;
+                    }
+                    stack.push(a[i]);
+                    break;
+                } else{
+                    stack.pop();
+                }
+            }
+        }
+        stack = new Stack<Integer>();
+        for (int i = N-1 ; i >= 0 ; i--){
+            while(true){
+                if(stack.isEmpty()){
+                    stack.push(a[i]);
+                    break;
+                }else if(stack.peek() < a[i]){
+                    int si = returnOperations(stack.peek(), a[i]);
+                    if(max < si){
+                        max = si;
+                    }
+                    stack.push(a[i]);
+                    break;
+                } else{
+                    stack.pop();
+                }
+			}
+        }
+    System.out.println(max);
+    }
+    public static int returnOperations(int M1 , int M2){
+        // you can directly use M1^M2 (equation converted to M1^M2)
         int mAnd = M1&M2;
         //System.out.println(mAnd);
         int mOr = M1|M2;
